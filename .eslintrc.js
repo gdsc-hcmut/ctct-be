577a2373bf8bbcdea2ed1959e672c15513d9cd15
Quoc Hung <truongquochung312@gmail.com>
2024-02-18 22:41:05 +0700

    feat: auto deploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     plugins: ["@typescript-eslint"],
     rules: {
-        '@typescript-eslint/no-explicit-any': 'warn',
-        '@typescript-eslint/no-unused-vars': 'warn',
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-unused-vars": "warn",
     },
 };
--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Template for backend services
 
 ## Running locally (automatically restarts)
+
 Test Test
+
 ```
 yarn dev
 ```
 
 ## Build project
+
 hello 2
+
 ```
 yarn build
 ```
